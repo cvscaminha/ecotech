716f9187407c36d123fcdc9bd7decae3c51441fc
cvscaminha <cvscaminha@hotmail.com>
2026-09-16 16:47:03 -0300

    Corrige dominio Render e configuracao de producao

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,4 +1,8 @@
 from .base import *
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.11.11.12']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ".onrender.com", "ecotech-nsou.onrender.com"]
 #ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ecotech-nsou.onrender.com",
+]
